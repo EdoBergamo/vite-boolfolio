d57e88d1dc75fffaf5a63090d18a40050264f214
EdoBergamo <edo.bergamo03@icloud.com>
2024-03-01 07:04:55 -0800

    chore: added api fetch

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+export default {
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  mounted() {
+    this.fetchProjects();
+  },
+  methods: {
+    async fetchProjects() {
+      try {
+        const response = await this.$axios.get('http://localhost:8000/api/projects');
+        this.projects = response.data.projects;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }
+}
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div v-if="projects.length > 0">
+      <ul>
+        <li v-for="project in projects" :key="project.id">
+          <h1>{{ project.name }}</h1>
+        </li>
+      </ul>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
